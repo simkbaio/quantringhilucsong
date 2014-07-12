a906e090e126420abe9c1552dd27a0372859247b
Duong Ngo Anh <simkbaio@yahoo.com>
2014-07-12 20:52:06 +0700

    update sample database

---
+++ sql/hosohocvien_db.sql
@@ -0,0 +1,222 @@
+/*
+SQLyog Ultimate v9.51 
+MySQL - 5.6.12-log : Database - hosohocvien_db
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `class_schedules` */
+
+DROP TABLE IF EXISTS `class_schedules`;
+
+CREATE TABLE `class_schedules` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `class_id` int(11) NOT NULL,
+  `hour_start` varchar(5) DEFAULT '00:00',
+  `hour_end` varchar(5) DEFAULT '00:00',
+  `day` int(2) DEFAULT '0',
+  `place` varchar(255) DEFAULT NULL,
+  `subject_id` int(11) unsigned NOT NULL,
+  `teacher_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `class_id` (`class_id`),
+  KEY `teacher_id` (`teacher_id`),
+  KEY `class_schedules_ibfk_2` (`subject_id`),
+  CONSTRAINT `class_schedules_ibfk_1` FOREIGN KEY (`class_id`) REFERENCES `tbl_class` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `class_schedules_ibfk_2` FOREIGN KEY (`subject_id`) REFERENCES `subjects` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `class_schedules_ibfk_3` FOREIGN KEY (`teacher_id`) REFERENCES `tbl_teacher` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Data for the table `class_schedules` */
+
+/*Table structure for table `subjects` */
+
+DROP TABLE IF EXISTS `subjects`;
+
+CREATE TABLE `subjects` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `subjects` */
+
+insert  into `subjects`(`id`,`name`,`description`,`created_at`,`updated_at`) values (3,'Tiếng Anh căn bản','Tiếng anh căn bản cho người chưa biết gì','2014-06-18 15:29:54','2014-06-18 15:29:54'),(4,'Tiếng anh chuyên ngành CNTT','Tiếng anh chuyên ngành CNTT','2014-06-18 15:37:51','2014-06-18 15:37:51'),(5,'Tập gõ 10 ngón','','2014-06-18 15:43:51','2014-06-18 15:43:51');
+
+/*Table structure for table `tbl_class` */
+
+DROP TABLE IF EXISTS `tbl_class`;
+
+CREATE TABLE `tbl_class` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) NOT NULL,
+  `description` text NOT NULL,
+  `course_id` int(11) NOT NULL,
+  `teacher_id` int(11) NOT NULL,
+  `start` int(11) DEFAULT NULL,
+  `end` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `tbl_class_ibfk_1` (`course_id`),
+  CONSTRAINT `tbl_class_ibfk_1` FOREIGN KEY (`course_id`) REFERENCES `tbl_course` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_class` */
+
+insert  into `tbl_class`(`id`,`name`,`description`,`course_id`,`teacher_id`,`start`,`end`) values (8,'Lập trình web căn bản - 3','Lập trình web căn bản - 3',3,4,NULL,NULL),(9,'Chỉnh sửa ảnh cơ bản','Chỉnh sửa ảnh đợt hè',2,4,NULL,NULL);
+
+/*Table structure for table `tbl_class_student` */
+
+DROP TABLE IF EXISTS `tbl_class_student`;
+
+CREATE TABLE `tbl_class_student` (
+  `class` int(11) NOT NULL,
+  `student` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  KEY `class` (`class`),
+  KEY `student` (`student`),
+  CONSTRAINT `tbl_class_student_ibfk_1` FOREIGN KEY (`class`) REFERENCES `tbl_class` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `tbl_class_student_ibfk_2` FOREIGN KEY (`student`) REFERENCES `tbl_student` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `tbl_class_student` */
+
+/*Table structure for table `tbl_course` */
+
+DROP TABLE IF EXISTS `tbl_course`;
+
+CREATE TABLE `tbl_course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `description` text NOT NULL,
+  `start` int(11) NOT NULL,
+  `end` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_course` */
+
+insert  into `tbl_course`(`id`,`name`,`description`,`start`,`end`) values (2,'Photoshop','Dạy chỉnh sửa ảnh bằng photoshop',1399334400,1433894400),(3,'Lập trình Web căn bản','Hướng dẫn căn bản về Lập trình Web: HTML, CSS, JS',1400605200,1423587600),(4,'Lập trình Web nâng cao','',1400630400,1407369600);
+
+/*Table structure for table `tbl_educated` */
+
+DROP TABLE IF EXISTS `tbl_educated`;
+
+CREATE TABLE `tbl_educated` (
+  `edu_id` int(11) NOT NULL AUTO_INCREMENT,
+  `edu_name` varchar(50) NOT NULL,
+  `edu_description` text NOT NULL,
+  PRIMARY KEY (`edu_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_educated` */
+
+/*Table structure for table `tbl_student` */
+
+DROP TABLE IF EXISTS `tbl_student`;
+
+CREATE TABLE `tbl_student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL,
+  `birthday` int(11) DEFAULT NULL,
+  `sex` int(1) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `hometown` varchar(255) DEFAULT NULL,
+  `phone` char(15) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `facebook` varchar(255) DEFAULT NULL,
+  `province_id` int(11) DEFAULT NULL,
+  `married` int(11) DEFAULT NULL,
+  `educated` int(11) DEFAULT NULL,
+  `type_disabilities` int(11) DEFAULT NULL,
+  `person_authen` varchar(250) DEFAULT NULL,
+  `person_authen_name` varchar(250) DEFAULT NULL,
+  `person_authen_address` varchar(250) DEFAULT NULL,
+  `regis_date` int(11) DEFAULT NULL,
+  `last_update` int(11) DEFAULT NULL,
+  `active` int(11) NOT NULL DEFAULT '1',
+  `password` varchar(255) DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `facebook_id` varchar(30) DEFAULT NULL,
+  `picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_student` */
+
+insert  into `tbl_student`(`id`,`name`,`birthday`,`sex`,`address`,`hometown`,`phone`,`email`,`facebook`,`province_id`,`married`,`educated`,`type_disabilities`,`person_authen`,`person_authen_name`,`person_authen_address`,`regis_date`,`last_update`,`active`,`password`,`last_name`,`first_name`,`facebook_id`,`picture`) values (8,'Ellie Wisoky',667612800,0,'170 Rosalyn Villages Apt. 785Conroystad, ME 94017','00966 Schumm Flat Apt. 728Brantville, RI 27158-4451','164-010-3656x99','keshawn.hintz@hotmail.com','fackebook.com/Tempore',1,0,3,1,NULL,'Tatyana Hirthe','2260 Howe UnionWest Abigailport, MS 27170-8507',NULL,NULL,1,NULL,NULL,NULL,NULL,NULL),(11,'Hoàng Văn An',24,0,'152 Xuân Đỉnh, Từ Liêm, Hà Nội','Hà Nội','0987654321','abc@abc.com','sdfsdf',1,1,2,1,NULL,'Hoàng Xuân Bê','152 Xuân Đỉnh, Từ Liêm, Hà Nội',1400492706,1400492706,1,NULL,NULL,NULL,NULL,NULL),(17,'Dương Hoàng Nam',635472000,0,'170 Rosalyn Villages Apt. 785Conroystad, ME 94017','00966 Schumm Flat Apt. 728Brantville, RI 27158-4451','20931023810','abc@abc.com','fackebook.com/Tempore',1,1,3,4,NULL,'Hoàng Xuân Bê','2260 Howe UnionWest Abigailport, MS 27170-8507',1400574157,1400574157,1,'e10adc3949ba59abbe56e057f20f883e',NULL,NULL,NULL,NULL),(18,'ádấdád',631126800,0,'adsađâsd','áđâsdá','0915320568','admin@domain.com','https://www.facebook.com/simkbaio',1,0,0,1,NULL,'đâsd','sdấd',1402299240,1402299240,1,'4297f44b13955235245b2497399d7a93',NULL,NULL,NULL,NULL),(22,'Dương Ngọc Anh',946659600,1,NULL,NULL,NULL,'simkbaio@gmail.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,'4297f44b13955235245b2497399d7a93','Dương','Ngọc Anh',NULL,NULL),(23,'',1,1,NULL,'Hanoi',NULL,'simkbaio@yahoo.com',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,1,NULL,'Ngọc Anh','Dương','10152443604284513',NULL);
+
+/*Table structure for table `tbl_student_result` */
+
+DROP TABLE IF EXISTS `tbl_student_result`;
+
+CREATE TABLE `tbl_student_result` (
+  `result_id` int(11) NOT NULL AUTO_INCREMENT,
+  `result_student_id` int(11) NOT NULL,
+  `result_class_id` int(11) NOT NULL,
+  `result_type_academic` int(11) DEFAULT '0',
+  `result_type_conduct` int(11) DEFAULT '0',
+  `result_note` text,
+  `status` int(1) NOT NULL DEFAULT '0',
+  `subject_id` int(11) unsigned NOT NULL,
+  `teacher_id` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`result_id`),
+  KEY `result_student_id` (`result_student_id`),
+  KEY `result_class_id` (`result_class_id`),
+  KEY `subject_id` (`subject_id`),
+  CONSTRAINT `tbl_student_result_ibfk_1` FOREIGN KEY (`result_student_id`) REFERENCES `tbl_student` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `tbl_student_result_ibfk_2` FOREIGN KEY (`result_class_id`) REFERENCES `tbl_class` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_student_result` */
+
+insert  into `tbl_student_result`(`result_id`,`result_student_id`,`result_class_id`,`result_type_academic`,`result_type_conduct`,`result_note`,`status`,`subject_id`,`teacher_id`) values (7,8,9,0,0,NULL,0,0,0),(8,11,9,0,0,NULL,0,0,0),(9,8,8,0,0,NULL,0,0,0),(10,17,8,0,0,NULL,0,0,0),(13,11,8,0,0,NULL,0,0,0),(14,17,9,0,0,NULL,0,0,0),(15,18,9,0,0,NULL,0,0,0),(16,18,8,0,0,NULL,0,0,0),(17,22,9,0,0,NULL,0,0,0),(18,22,8,0,0,NULL,0,0,0);
+
+/*Table structure for table `tbl_teacher` */
+
+DROP TABLE IF EXISTS `tbl_teacher`;
+
+CREATE TABLE `tbl_teacher` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `type` int(11) DEFAULT NULL,
+  `join_date` int(11) DEFAULT NULL,
+  `out_date` int(11) DEFAULT NULL,
+  `phone` char(50) DEFAULT NULL,
+  `address` varchar(250) DEFAULT NULL,
+  `email` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_teacher` */
+
+insert  into `tbl_teacher`(`id`,`name`,`type`,`join_date`,`out_date`,`phone`,`address`,`email`) values (4,'Dương Ngọc Anh',NULL,1363824000,1426896000,'0984564567','Hà Nội','simkbaio@gmail.com'),(5,'Trần Minh Hiếu',NULL,1391360400,1422896400,'09892312','172 Xuân Đỉnh, Từ Liêm, Hà Nội','simkbaio@12abc.com');
+
+/*Table structure for table `tbl_types_disabilities` */
+
+DROP TABLE IF EXISTS `tbl_types_disabilities`;
+
+CREATE TABLE `tbl_types_disabilities` (
+  `dis_id` int(11) NOT NULL AUTO_INCREMENT,
+  `dis_name` varchar(100) NOT NULL,
+  `dis_description` text NOT NULL,
+  PRIMARY KEY (`dis_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/*Data for the table `tbl_types_disabilities` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
---
+++ sql/user_api_db.sql
@@ -0,0 +1,214 @@
+/*
+SQLyog Ultimate v9.51 
+MySQL - 5.6.12-log : Database - users_api
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `groups` */
+
+DROP TABLE IF EXISTS `groups`;
+
+CREATE TABLE `groups` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `permissions` text COLLATE utf8_unicode_ci,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `groups_name_unique` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `groups` */
+
+insert  into `groups`(`id`,`name`,`permissions`,`created_at`,`updated_at`) values (2,'Thành viên thông thường','{\"user\":1,\"admin\":1,\"admin.menuspack.index\":1}','0000-00-00 00:00:00','2014-05-16 08:50:11'),(6,'Quản trị viên','{\"admin\":1,\"admin.menuspack.index\":1,\"admin.menuspacks.index\":1,\"admin.students.create\":1,\"admin.students.index\":1,\"user\":1}','2014-05-15 21:48:08','2014-05-20 08:51:09'),(7,'Học viên nghị lực sống','{\"admin.menuspack.index\":1,\"user\":1}','2014-05-15 22:16:47','2014-05-16 09:13:12'),(8,'Giáo viên Nghị Lực Sống','{\"user\":1}','2014-05-15 22:17:17','2014-05-16 09:15:56'),(9,'Developers','{\"admin\":1,\"admin.menuspack.index\":1,\"admin.menuspacks.index\":1,\"admin.students.create\":1,\"admin.students.index\":1,\"user\":1}','2014-05-17 10:39:25','2014-05-20 09:22:54');
+
+/*Table structure for table `menu_menuspack` */
+
+DROP TABLE IF EXISTS `menu_menuspack`;
+
+CREATE TABLE `menu_menuspack` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `menu_id` int(10) unsigned NOT NULL,
+  `menuspack_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  KEY `menu_menuspack_menu_id_index` (`menu_id`),
+  KEY `menu_menuspack_menuspack_id_index` (`menuspack_id`),
+  CONSTRAINT `menu_menuspack_menuspack_id_foreign` FOREIGN KEY (`menuspack_id`) REFERENCES `menuspacks` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `menu_menuspack_menu_id_foreign` FOREIGN KEY (`menu_id`) REFERENCES `menus` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `menu_menuspack` */
+
+/*Table structure for table `menus` */
+
+DROP TABLE IF EXISTS `menus`;
+
+CREATE TABLE `menus` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `parent` int(11) NOT NULL DEFAULT '0',
+  `menuspack_id` int(11) NOT NULL DEFAULT '0',
+  `order` int(11) NOT NULL DEFAULT '0',
+  `link` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '#',
+  `sercury` tinyint(1) NOT NULL DEFAULT '0',
+  `icon` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `permission` text COLLATE utf8_unicode_ci,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `menus` */
+
+insert  into `menus`(`id`,`title`,`parent`,`menuspack_id`,`order`,`link`,`sercury`,`icon`,`description`,`permission`,`created_at`,`updated_at`) values (2,'Quản lý Thành viên',0,1,2,'#',0,'fa fa-group',NULL,NULL,'2014-05-13 09:51:18','2014-05-25 07:37:31'),(6,'Trang chủ',0,1,0,'admin',0,'fa fa-home',NULL,NULL,'2014-05-14 09:41:10','2014-05-25 07:37:31'),(9,'Config',0,1,3,'#',0,'fa fa-gears ',NULL,NULL,'2014-05-14 10:19:50','2014-05-25 07:37:31'),(12,'Nhóm',2,1,0,'admin/groups',0,'fa fa-group',NULL,NULL,'2014-05-15 09:00:49','2014-05-25 07:37:31'),(13,'Thành viên',2,1,1,'admin/users',0,'fa fa-user',NULL,NULL,'2014-05-15 09:06:35','2014-05-25 07:37:31'),(14,'Menus',9,1,0,'admin/menuspacks',0,'fa fa-tasks',NULL,NULL,'2014-05-15 09:08:30','2014-05-25 07:37:31'),(15,'Quyền truy cập',2,1,2,'admin/permissions',0,'fa fa-lock',NULL,NULL,'2014-05-16 09:25:07','2014-05-25 07:37:31'),(16,'Quản lý học tập',0,1,1,'#',0,'fa fa-building-o',NULL,NULL,'2014-05-19 05:07:53','2014-05-25 07:37:31'),(17,'Học viên',16,1,3,'admin/students',0,'fa fa-book',NULL,NULL,'2014-05-19 05:09:58','2014-05-25 07:37:31'),(18,'Giáo viên',16,1,2,'admin/teachers',0,'fa fa-briefcase',NULL,NULL,'2014-05-20 09:17:23','2014-05-25 07:37:31'),(19,'Khóa học',16,1,0,'admin/courses',0,'fa fa-leaf',NULL,NULL,'2014-05-21 09:03:54','2014-05-25 07:37:31'),(20,'Lớp học',16,1,1,'admin/classes',0,'fa fa-coffee',NULL,NULL,'2014-05-22 08:02:59','2014-05-25 07:37:31');
+
+/*Table structure for table `menuspacks` */
+
+DROP TABLE IF EXISTS `menuspacks`;
+
+CREATE TABLE `menuspacks` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `order` int(11) NOT NULL DEFAULT '0',
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `menuspacks` */
+
+insert  into `menuspacks`(`id`,`name`,`order`,`created_at`,`updated_at`) values (1,'Admin: Left Sidebar',0,'2014-05-13 09:51:18','2014-05-13 09:51:18'),(2,'Menu trang chủ',0,'2014-05-15 21:50:18','2014-05-15 21:50:18');
+
+/*Table structure for table `migrations` */
+
+DROP TABLE IF EXISTS `migrations`;
+
+CREATE TABLE `migrations` (
+  `migration` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `migrations` */
+
+insert  into `migrations`(`migration`,`batch`) values ('2012_12_06_225921_migration_cartalyst_sentry_install_users',1),('2012_12_06_225929_migration_cartalyst_sentry_install_groups',1),('2012_12_06_225945_migration_cartalyst_sentry_install_users_groups_pivot',1),('2012_12_06_225988_migration_cartalyst_sentry_install_throttle',1),('2014_05_11_201752_create_permissions_table',2),('2014_05_12_095049_create_menuspacks_table',3),('2014_05_12_095932_create_menus_table',3),('2014_05_12_102758_create_menu_menuspack_table',3),('2014_05_16_094357_create_tblTeacher_table',4),('2014_05_31_024313_Create_Pages_Table',5),('2014_06_12_160302_create_subjects_table',6);
+
+/*Table structure for table `pages` */
+
+DROP TABLE IF EXISTS `pages`;
+
+CREATE TABLE `pages` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `slug` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `file` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `homepage` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT '0',
+  `meta_title` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_description` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `meta_keyword` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `robots` varchar(255) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'index,follow',
+  `actived` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `pages` */
+
+/*Table structure for table `permissions` */
+
+DROP TABLE IF EXISTS `permissions`;
+
+CREATE TABLE `permissions` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `permissions` */
+
+insert  into `permissions`(`id`,`name`,`created_at`,`updated_at`) values (3,'admin','2014-05-15 20:57:44','2014-05-15 20:57:44'),(4,'user','2014-05-15 20:57:44','2014-05-15 20:57:44'),(5,'admin.menuspack.index','2014-05-20 04:38:34','2014-05-20 04:38:34'),(6,'admin.students.index','2014-05-20 04:39:24','2014-05-20 04:39:24'),(7,'admin.students.create','2014-05-20 04:43:13','2014-05-20 04:43:13'),(8,'admin.menuspacks.index','2014-05-20 08:50:40','2014-05-20 08:50:40');
+
+/*Table structure for table `throttle` */
+
+DROP TABLE IF EXISTS `throttle`;
+
+CREATE TABLE `throttle` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned DEFAULT NULL,
+  `ip_address` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `attempts` int(11) NOT NULL DEFAULT '0',
+  `suspended` tinyint(1) NOT NULL DEFAULT '0',
+  `banned` tinyint(1) NOT NULL DEFAULT '0',
+  `last_attempt_at` timestamp NULL DEFAULT NULL,
+  `suspended_at` timestamp NULL DEFAULT NULL,
+  `banned_at` timestamp NULL DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `throttle_user_id_index` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `throttle` */
+
+insert  into `throttle`(`id`,`user_id`,`ip_address`,`attempts`,`suspended`,`banned`,`last_attempt_at`,`suspended_at`,`banned_at`) values (1,25,'127.0.0.1',0,0,0,NULL,NULL,NULL),(2,27,'127.0.0.1',0,0,0,NULL,NULL,NULL);
+
+/*Table structure for table `users` */
+
+DROP TABLE IF EXISTS `users`;
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `permissions` text COLLATE utf8_unicode_ci,
+  `activated` tinyint(1) NOT NULL DEFAULT '0',
+  `activation_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `activated_at` timestamp NULL DEFAULT NULL,
+  `last_login` timestamp NULL DEFAULT NULL,
+  `persist_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `reset_password_code` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `first_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `last_name` varchar(255) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `users_email_unique` (`email`),
+  KEY `users_activation_code_index` (`activation_code`),
+  KEY `users_reset_password_code_index` (`reset_password_code`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users` */
+
+insert  into `users`(`id`,`email`,`password`,`permissions`,`activated`,`activation_code`,`activated_at`,`last_login`,`persist_code`,`reset_password_code`,`first_name`,`last_name`,`created_at`,`updated_at`) values (27,'simkbaio@gmail.com','$2y$10$nHfg2TgWGXSMvU5Ur9LkJeGwNg37usrtwEJjOW1dRhifyYcNrrSem','',1,NULL,NULL,'2014-06-26 16:36:03','$2y$10$7HcOVACl57SPMhzJWLDiZePpjZNJVxZV2w1O7meueSYp/utUcdkB2',NULL,'Ngọc Anh','Dương','2014-05-16 04:39:39','2014-06-26 16:36:03');
+
+/*Table structure for table `users_groups` */
+
+DROP TABLE IF EXISTS `users_groups`;
+
+CREATE TABLE `users_groups` (
+  `user_id` int(10) unsigned NOT NULL,
+  `group_id` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`user_id`,`group_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+/*Data for the table `users_groups` */
+
+insert  into `users_groups`(`user_id`,`group_id`) values (27,2),(27,6),(27,9);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
